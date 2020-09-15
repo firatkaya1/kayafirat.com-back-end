2310a28a9745f49eeaecbaf6b3ace169877dd2b4
firatkaya1 <firat.kaya@mail.com>
2020-09-15 20:15:10 +0300

    added hikari configs

---
+++ src/main/java/com/firatkaya/config/HikariConfig.java
@@ -0,0 +1,18 @@
+package com.firatkaya.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@Configuration
+@ConfigurationProperties(prefix="spring.datasource.hikari")
+public class HikariConfig extends com.zaxxer.hikari.HikariConfig {
+    @Bean
+    public DataSource dataSource() throws SQLException {
+        return new HikariDataSource(this);
+    }
+}
