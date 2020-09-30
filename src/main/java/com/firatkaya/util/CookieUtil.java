daac8b69ba3b801ba620fbfdd0b3cb5d8206676c
firatkaya1 <firat.kaya@mail.com>
2020-10-01 00:15:01 +0300

    added CookieUtil class

---
+++ src/main/java/com/firatkaya/util/CookieUtil.java
@@ -0,0 +1,13 @@
+package com.firatkaya.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    public String createCookie(String key,String value){
+
+        return null;
+    }
+
+}
